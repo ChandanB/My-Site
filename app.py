235ba3d53234cb25701c9628f0ac0cfdcf58e2c8
ChandanB <chandanbrown@yahoo.com>
2017-02-23 16:02:39 -0800

    HTML

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 import os
+import codecs
+
 app = Flask(__name__)
 
 @app.route('/')
 def get_Random_word():
-    print("W")
-    return 'LOLLL'
+    f=codecs.open("index.html", 'r')
+    return f.read()
 
 if __name__ == '__main__':
+    f = open('index.html','w')
     #uncomment the next two lines to run on heroku
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
